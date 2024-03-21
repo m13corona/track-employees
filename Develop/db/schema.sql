f803fdef0655f643b726eb95c8cd21048eff37d8
m13corona <mcorona1308@gmail.com>
2024-03-21 16:44:01 -0600

    created table

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -5,12 +5,19 @@ USE employees_db;
 
 CREATE TABLE department (
 
-    id INT NOT NULL,
-    name VARCHAR(30) --DEPARTMENT NAME--
+    id INT PRIMARY KEY,
+    name VARCHAR(30) 
 
 );
 
 INSERT INTO
     department (id, name)
 VALUES
-()
+(1, "Operations"),
+(2, "Sales"),
+(3, "Finance");
+
+SELECT * FROM department;
+
+
+CREATE TABLE 
