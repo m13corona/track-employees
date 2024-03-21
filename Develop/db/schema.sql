b4d937bc4acb63a0f1345df07df666c79befe756
m13corona <mcorona1308@gmail.com>
2024-03-21 16:32:17 -0600

    add code to schema.sql

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -1,1 +1,16 @@
-DROP DATABASE IF EXISTS 
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+
+    id INT NOT NULL,
+    name VARCHAR(30) --DEPARTMENT NAME--
+
+);
+
+INSERT INTO
+    department (id, name)
+VALUES
+()
